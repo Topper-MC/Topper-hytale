5c6b6fae911c07e8cce1c8c4ec62e762ef347124
hsgamer <huynhqtienvtag@gmail.com>
2026-02-07 08:14:40 +0700

    complete permission filter

--- src/main/java/me/hsgamer/topperstale/template/HyTopHolderSettings.java
+++ src/main/java/me/hsgamer/topperstale/template/HyTopHolderSettings.java
@@ -1,13 +1,26 @@
 package me.hsgamer.topperstale.template;
 
+import com.hypixel.hytale.server.core.permissions.PermissionsModule;
+import me.hsgamer.hscore.common.CollectionUtils;
 import me.hsgamer.topper.agent.update.UpdateAgent;
 import me.hsgamer.topper.template.topplayernumber.holder.NumberTopHolder;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-public record HyTopHolderSettings(Map<String, Object> map) implements NumberTopHolder.Settings {
+public final class HyTopHolderSettings implements NumberTopHolder.Settings {
+    private final Map<String, Object> map;
+    private final List<String> ignorePermissions;
+    private final List<String> resetPermissions;
+
+    public HyTopHolderSettings(Map<String, Object> map) {
+        this.map = map;
+        ignorePermissions = CollectionUtils.createStringListFromObject(map.get("ignore-permission"), true);
+        resetPermissions = CollectionUtils.createStringListFromObject(map.get("reset-permission"), true);
+    }
+
     @Override
     public Double defaultValue() {
         return Optional.ofNullable(map.get("default-value"))
@@ -71,12 +84,25 @@ public record HyTopHolderSettings(Map<String, Object> map) implements NumberTopH
 
     @Override
     public UpdateAgent.FilterResult filter(UUID uuid) {
-        // TODO: Filter based on Permissions
+        if (ignorePermissions.isEmpty() && resetPermissions.isEmpty()) {
+            return UpdateAgent.FilterResult.CONTINUE;
+        }
+        PermissionsModule module = PermissionsModule.get();
+        if (!resetPermissions.isEmpty() && resetPermissions.stream().anyMatch(permission -> module.hasPermission(uuid, permission))) {
+            return UpdateAgent.FilterResult.RESET;
+        }
+        if (!ignorePermissions.isEmpty() && ignorePermissions.stream().anyMatch(permission -> module.hasPermission(uuid, permission))) {
+            return UpdateAgent.FilterResult.SKIP;
+        }
         return UpdateAgent.FilterResult.CONTINUE;
     }
 
     @Override
     public Map<String, Object> valueProvider() {
+        return map;
+    }
+
+    public Map<String, Object> map() {
         return map;
     }
 }
