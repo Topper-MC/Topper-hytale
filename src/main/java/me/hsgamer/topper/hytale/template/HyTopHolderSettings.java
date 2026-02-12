f057e46c675a4d8a94442420222044f5371903cf
hsgamer <huynhqtienvtag@gmail.com>
2026-02-12 23:15:08 +0700

    update topper usage

--- src/main/java/me/hsgamer/topper/hytale/template/HyTopHolderSettings.java
+++ src/main/java/me/hsgamer/topper/hytale/template/HyTopHolderSettings.java
@@ -7,79 +7,16 @@ import me.hsgamer.topper.template.topplayernumber.holder.NumberTopHolder;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
-public final class HyTopHolderSettings implements NumberTopHolder.Settings {
-    private final Map<String, Object> map;
+public final class HyTopHolderSettings extends NumberTopHolder.MapSettings {
     private final List<String> ignorePermissions;
     private final List<String> resetPermissions;
 
     public HyTopHolderSettings(Map<String, Object> map) {
-        this.map = map;
+        super(map);
         ignorePermissions = CollectionUtils.createStringListFromObject(map.get("ignore-permission"), true);
         resetPermissions = CollectionUtils.createStringListFromObject(map.get("reset-permission"), true);
-    }
-
-    @Override
-    public Double defaultValue() {
-        return Optional.ofNullable(map.get("default-value"))
-                .map(Object::toString)
-                .map(s -> {
-                    try {
-                        return Double.parseDouble(s);
-                    } catch (NumberFormatException e) {
-                        return null;
-                    }
-                })
-                .orElse(null);
-    }
-
-    @Override
-    public String displayNullName() {
-        return Optional.ofNullable(map.get("null-name"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
-    @Override
-    public String displayNullUuid() {
-        return Optional.ofNullable(map.get("null-uuid"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
-    @Override
-    public String displayNullValue() {
-        return Optional.ofNullable(map.get("null-value"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
-    @Override
-    public boolean showErrors() {
-        return Optional.ofNullable(map.get("show-errors"))
-                .map(Object::toString)
-                .map(String::toLowerCase)
-                .map(Boolean::parseBoolean)
-                .orElse(false);
-    }
-
-    @Override
-    public boolean resetOnError() {
-        return Optional.ofNullable(map.get("reset-on-error"))
-                .map(Object::toString)
-                .map(String::toLowerCase)
-                .map(Boolean::parseBoolean)
-                .orElse(true);
-    }
-
-    @Override
-    public boolean reverse() {
-        return Optional.ofNullable(map.get("reverse"))
-                .map(String::valueOf)
-                .map(Boolean::parseBoolean)
-                .orElse(false);
     }
 
     @Override
@@ -95,14 +32,5 @@ public final class HyTopHolderSettings implements NumberTopHolder.Settings {
             return UpdateAgent.FilterResult.SKIP;
         }
         return UpdateAgent.FilterResult.CONTINUE;
-    }
-
-    @Override
-    public Map<String, Object> valueProvider() {
-        return map;
-    }
-
-    public Map<String, Object> map() {
-        return map;
     }
 }
